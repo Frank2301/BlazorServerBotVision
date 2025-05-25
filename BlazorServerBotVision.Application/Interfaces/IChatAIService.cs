8d39d70b4395fc762994e4c042fb6271a875d596
Frank K. <info@eworxx.com>
2025-05-25 15:01:47 +0200

    milestone v2.0 - ai integration

---
+++ BlazorServerBotVision.Application/Interfaces/IChatAIService.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace BlazorServerBotVision.Application.Interfaces
+{
+    public interface IChatAIService
+    {
+        Task<string> GetAiGeneratedChatAsync(string userPrompt);
+    }
+}
